a2d59d87eabf2428f22ae84a545796f01f70e22c
ballqs <46590521+ballqs@users.noreply.github.com>
2024-07-24 13:54:37 +0900

    2-7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,53 +8,83 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Calculator calculator = new Calculator(new ArrayList<>());
+        // 초기값을 뭘 넣어야할지 몰라서 아래와 같이 처리
+        Calculator calculator = new Calculator(new ArrayList<>() , new ArrayList<>());
 
-        ArrayList<Integer> result = calculator.getResult();
+        // 2-7. 숫자로 분기하는게 편해서 숫자로 진행했습니다.
+        System.out.println("(1)사칙연산 , (2)원의넓이 중 고르세요!");
+        System.out.print(" >> ");
+        int bifurcation = sc.nextInt();
 
-        while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+        if (bifurcation == 1) {
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char sign = sc.next().charAt(0);
+            ArrayList<Integer> result = calculator.getResult();
 
-            // 이거 2-1에 추가하는줄 알고 넣었는데 2-2로 커밋하기 위해 다시 구조 바꾸겠습니다.
-            int calc = calculator.calculate(num1 , num2 , sign);
-            result.add(calc);
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = sc.nextInt();
 
-            System.out.println("결과: " + result.get(result.size() - 1));
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char sign = sc.next().charAt(0);
 
-            /**
-             * 사용자가 콘솔창에 값을 입력 후 enter를 누르는데 이때 개행문자는 제거하지 않은 채 그대로 남겨두기에
-             * 남겨진 개행문자가 다음 sc.nextLine() 입력으로 처리되어 곧바로 다음 라인으로 넘어가기에 추가함
-             * */
-            sc.nextLine();
+                // 이거 2-1에 추가하는줄 알고 넣었는데 2-2로 커밋하기 위해 다시 구조 바꾸겠습니다.
+                int calc = calculator.calculate(num1 , num2 , sign);
+                result.add(calc);
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            System.out.print(" >> ");
-            String remove_confirm = sc.nextLine();
-            if (remove_confirm.equals("remove")) {
-                // 여기서 삭제한 값이 위에 선언된 result에 적용되는 이유는
-                // 같은 인스턴스 주소인 calculator에서 받아왔기 때문이다.
-                calculator.removeResult();
+                System.out.println("결과: " + result.get(result.size() - 1));
+
+                /**
+                 * 사용자가 콘솔창에 값을 입력 후 enter를 누르는데 이때 개행문자는 제거하지 않은 채 그대로 남겨두기에
+                 * 남겨진 개행문자가 다음 sc.nextLine() 입력으로 처리되어 곧바로 다음 라인으로 넘어가기에 추가함
+                 * */
+                sc.nextLine();
+
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                System.out.print(" >> ");
+                String remove_confirm = sc.nextLine();
+                if (remove_confirm.equals("remove")) {
+                    // 여기서 삭제한 값이 위에 선언된 result에 적용되는 이유는
+                    // 같은 인스턴스 주소인 calculator에서 받아왔기 때문이다.
+                    calculator.removeResult();
+                }
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                System.out.print(" >> ");
+                String select_confirm = sc.nextLine();
+                if (select_confirm.equals("inquiry")) {
+                    calculator.inquiryResults();
+                }
+
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                System.out.print(" >> ");
+                String confirm = sc.nextLine();
+                if (confirm.equals("exit")) {
+                    calculator.setResult(result);
+                    break;
+                }
             }
 
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            System.out.print(" >> ");
-            String select_confirm = sc.nextLine();
-            if (select_confirm.equals("inquiry")) {
-                calculator.inquiryResults();
-            }
+        } else {
+            ArrayList<Double> result2 = calculator.getResult2();
 
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            System.out.print(" >> ");
-            String confirm = sc.nextLine();
-            if (confirm.equals("exit")) {
-                calculator.setResult(result);
-                break;
+            while (true) {
+                System.out.print("반지름 길이를 입력하세요: ");
+                double radius = sc.nextDouble();
+
+                // 원의 넓이 결과값
+                result2.add(calculator.calculateCircleArea(radius));
+                System.out.println("결과: " + result2.get(result2.size() - 1));
+
+                sc.nextLine();
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                System.out.print(" >> ");
+                String confirm = sc.nextLine();
+                if (confirm.equals("exit")) {
+                    calculator.setResult2(result2);
+                    break;
+                }
             }
         }
     }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,12 +3,18 @@ package calculator;
 import calculator.exception.CalculatorException;
 import java.util.ArrayList;
 
-public class Calculator {
+// 2-7 외부에서 접근 불가라는 뜻을 클래스 전체에 적용하는건가? 싶어서 public -> default로 변경
+class Calculator {
     private ArrayList<Integer> result;
 
+    // 2-7. 변하지 않아야하는 상수값이어야하고 final를! 3.14라서 double형을 사용합니다.
+    final double PI = 3.14;
+    private ArrayList<Double> result2;
+
     // 2-6 Calculator 인스턴스를 생성할 때 생성자를 통해 연산 결과를 저장하고 있는 필드 초기화
-    Calculator(ArrayList<Integer> result) {
-        this.result = result;
+    Calculator(ArrayList<Integer> result , ArrayList<Double> result2) {
+        this.result = result;   // 사칙연산값을 저장하는 곳
+        this.result2 = result2; // 원의넓이값을 저장하는 곳
     }
 
     public int calculate(int num1 , int num2 , char sign) {
@@ -53,11 +59,28 @@ public class Calculator {
         }
     }
 
+    // 2-7 원의 넓이를 구하는 메서드
+    public double calculateCircleArea(double radius) {
+        return 2 * radius * PI;
+    }
+
+    // 사칙연산 저장값 get
     public ArrayList<Integer> getResult() {
         return result;
     }
 
+    // 사칙연산 저장값 set
     public void setResult(ArrayList<Integer> result) {
         this.result = result;
+    }
+
+    // 원의넓이 저장값 get
+    public ArrayList<Double> getResult2() {
+        return result2;
+    }
+
+    // 원의넓이 저장값 set
+    public void setResult2(ArrayList<Double> result2) {
+        this.result2 = result2;
     }
 }
