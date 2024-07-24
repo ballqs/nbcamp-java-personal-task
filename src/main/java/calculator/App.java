1420055d70eb0509022b7872548a49cfb13560cc
ballqs <46590521+ballqs@users.noreply.github.com>
2024-07-24 10:55:20 +0900

    1-4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,39 +7,54 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char sign = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char sign = sc.next().charAt(0);
 
-        int result = 0;
+            int result = 0;
 
-        switch (sign) {
-            case '+' :
-                result = num1 + num2;
+            switch (sign) {
+                case '+' :
+                    result = num1 + num2;
+                    break;
+                case '-' :
+                    result = num1 - num2;
+                    break;
+                case '*' :
+                    result = num1 * num2;
+                    break;
+                case '/' :
+                    // 1-3때 지문을 잘못이해하여 다시 수정
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
+                    }else {
+                        result = num1 / num2;
+                    }
+                    break;
+                case '%' :
+                    result = num1 % num2;
+                    break;
+            }
+
+            System.out.println("결과: " + result);
+
+            /**
+             * 사용자가 콘솔창에 값을 입력 후 enter를 누르는데 이때 개행문자는 제거하지 않은 채 그대로 남겨두기에
+             * 남겨진 개행문자가 다음 sc.nextLine() 입력으로 처리되어 곧바로 다음 라인으로 넘어가기에 추가함
+             * */
+            sc.nextLine();
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.print(" >> ");
+            String confirm = sc.nextLine();
+            if (confirm.equals("exit")) {
                 break;
-            case '-' :
-                result = num1 - num2;
-                break;
-            case '*' :
-                result = num1 * num2;
-                break;
-            case '/' :
-                // 1-3때 지문을 잘못이해하여 다시 수정
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
-                }else {
-                    result = num1 / num2;
-                }
-                break;
-            case '%' :
-                result = num1 % num2;
-                break;
+            }
         }
-
-        System.out.println("결과: " + result);
     }
 }
