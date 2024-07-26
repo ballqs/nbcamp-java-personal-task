131fae23e5a5e2e17d74023da13c3a3827a65969
ballqs <46590521+ballqs@users.noreply.github.com>
2024-07-26 11:23:23 +0900

    3-1

--- src/main/java/calculator/ArithmeticCalculator.java
+++ src/main/java/calculator/ArithmeticCalculator.java
@@ -6,20 +6,19 @@ import java.util.ArrayList;
 
 // 사칙연산
 public class ArithmeticCalculator extends Calculator<OperatorInterface , Character>{
-
     @Override
     public OperatorInterface calculate(Character sign) {
         try {
-            switch (sign) {
-                case '+' :
+            switch (SignReplace(sign).name()) {
+                case "PLUS" :
                     return new AddOperator();
-                case '-' :
+                case "MINUS" :
                     return new SubtractOperator();
-                case '*' :
+                case "MULTI" :
                     return new MultiplyOperator();
-                case '/' :
+                case "DIVIDE" :
                     return new DivideOperator();
-                case '%' :
+                case "MOD" :
                     return new ModOperator();
                 default:
                     throw new CalculatorException();
@@ -28,6 +27,24 @@ public class ArithmeticCalculator extends Calculator<OperatorInterface , Charact
             System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. ");
         } catch (CalculatorException e) { // 적합한 예외처리가 뭔지 몰라 작성함
             System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    // 기존 사칙연산에서 사용중이던 기호를 버리고 문자열로 받을까?
+    // 고민하다가 그냥 아래와 같이 처리
+    public SignType SignReplace(Character sign) {
+        switch (sign) {
+            case '+' :
+                return SignType.PLUS;
+            case '-' :
+                return SignType.MINUS;
+            case '*' :
+                return SignType.MULTI;
+            case '/' :
+                return SignType.DIVIDE;
+            case '%' :
+                return SignType.MOD;
         }
         return null;
     }
---
+++ src/main/java/calculator/SignType.java
@@ -0,0 +1,6 @@
+package calculator;
+
+// Enum을 별도의 .java로 선언할수 있다고 나와서 선언
+public enum SignType {
+    PLUS , MINUS , MULTI , DIVIDE , MOD
+}
